6e78fdddcef86471623cfb7fcc006e3cd6067384
Anthony Chamberland <anthochamberland19@outlook.com>
2025-06-22 14:09:29 -0400

    Add stream reader and writer tests

---
+++ tests/FsgImg.Tests/IO/ImgHeaderStreamReaderTest.cs
@@ -0,0 +1,122 @@
+﻿using FsgImg.Abstractions;
+using FsgImg.Abstractions.Enums;
+using FsgImg.Abstractions.Interfaces;
+using FsgImg.Abstractions.Interfaces.Factories;
+using FsgImg.Factories;
+using System.Buffers;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FsgImg.Tests.IO
+{
+    public class ImgHeaderStreamReaderTest
+    {
+        protected virtual IImgHeaderStreamReaderFactory Factory { get; } =
+            new ImgHeaderStreamReaderFactory(new ImgHeaderBufferReaderFactory());
+
+        [Fact]
+        public void Read_Xbox360Bc1_ReturnsImgHeader()
+        {
+            // Arrange
+            IImgHeader imgHeader;
+            var buffer = ArrayPool<byte>.Shared.Rent(ImgConstants.ImgHeaderSize);
+            try
+            {
+                buffer[0] = 0x02;
+                buffer[1] = 0x00;
+                buffer[2] = 0x08;
+                buffer[3] = 0x00;
+                buffer[4] = 0x00;
+                buffer[5] = 0x01;
+                buffer[6] = 0x02;
+                buffer[7] = 0x00;
+                buffer[8] = 0x00;
+                buffer[9] = 0x00;
+                buffer[10] = 0x00;
+                buffer[11] = 0x05;
+                buffer[12] = 0x00;
+                buffer[13] = 0x00;
+                buffer[14] = 0x01;
+                buffer[15] = 0x00;
+                buffer[16] = 0x00;
+                buffer[17] = 0x06;
+                buffer[18] = 0x03;
+                buffer[19] = 0x00;
+
+                using var stream = new MemoryStream(buffer, 0, ImgConstants.ImgHeaderSize, false);
+                using var reader = Factory.Create(stream, true);
+
+                // Act
+                imgHeader = reader.Read();
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+
+            // Assert
+            Assert.Equal(512, imgHeader.Width);
+            Assert.Equal(2048, imgHeader.Height);
+            Assert.Equal(1, imgHeader.Depth);
+            Assert.Equal(512, imgHeader.Pitch);
+            Assert.Equal(ImgTextureFormat.Bc1, imgHeader.TextureFormat);
+            Assert.Equal(0, imgHeader.BcAlpha);
+            Assert.Equal(ImgGame.ConsoleGhl, imgHeader.Game);
+            Assert.Equal(7, imgHeader.MipmapCount);
+            Assert.Equal(ImgPlatform.Xbox360, imgHeader.Platform);
+        }
+
+        [Fact]
+        public async Task ReadAsync_Xbox360Bc1_ReturnsImgHeader()
+        {
+            // Arrange
+            IImgHeader imgHeader;
+            var buffer = ArrayPool<byte>.Shared.Rent(ImgConstants.ImgHeaderSize);
+            try
+            {
+                buffer[0] = 0x02;
+                buffer[1] = 0x00;
+                buffer[2] = 0x08;
+                buffer[3] = 0x00;
+                buffer[4] = 0x00;
+                buffer[5] = 0x01;
+                buffer[6] = 0x02;
+                buffer[7] = 0x00;
+                buffer[8] = 0x00;
+                buffer[9] = 0x00;
+                buffer[10] = 0x00;
+                buffer[11] = 0x05;
+                buffer[12] = 0x00;
+                buffer[13] = 0x00;
+                buffer[14] = 0x01;
+                buffer[15] = 0x00;
+                buffer[16] = 0x00;
+                buffer[17] = 0x06;
+                buffer[18] = 0x03;
+                buffer[19] = 0x00;
+
+                using var stream = new MemoryStream(buffer, 0, ImgConstants.ImgHeaderSize, false);
+                using var reader = Factory.Create(stream, true);
+
+                // Act
+                imgHeader = await reader.ReadAsync();
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+
+            // Assert
+            Assert.Equal(512, imgHeader.Width);
+            Assert.Equal(2048, imgHeader.Height);
+            Assert.Equal(1, imgHeader.Depth);
+            Assert.Equal(512, imgHeader.Pitch);
+            Assert.Equal(ImgTextureFormat.Bc1, imgHeader.TextureFormat);
+            Assert.Equal(0, imgHeader.BcAlpha);
+            Assert.Equal(ImgGame.ConsoleGhl, imgHeader.Game);
+            Assert.Equal(7, imgHeader.MipmapCount);
+            Assert.Equal(ImgPlatform.Xbox360, imgHeader.Platform);
+        }
+    }
+}
---
+++ tests/FsgImg.Tests/IO/ImgHeaderStreamWriterTest.cs
@@ -0,0 +1,150 @@
+﻿using FsgImg.Abstractions;
+using FsgImg.Abstractions.Enums;
+using FsgImg.Abstractions.Interfaces.Factories;
+using FsgImg.Factories;
+using System;
+using System.Buffers;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FsgImg.Tests.IO
+{
+    public class ImgHeaderStreamWriterTest
+    {
+        protected virtual IImgHeaderStreamWriterFactory Factory { get; } =
+            new ImgHeaderStreamWriterFactory(new ImgHeaderBufferWriterFactory());
+
+        [Fact]
+        public void Write_Xbox360Bc1_WritesImgHeaderToStream()
+        {
+            // Arrange
+            var imgHeader = new ImgHeader
+            {
+                Width = 512,
+                Height = 2048,
+                Depth = 1,
+                Pitch = 512,
+                TextureFormat = ImgTextureFormat.Bc1,
+                BcAlpha = 0,
+                Game = ImgGame.ConsoleGhl,
+                MipmapCount = 7,
+                Platform = ImgPlatform.Xbox360
+            };
+
+            var actual = ArrayPool<byte>.Shared.Rent(ImgConstants.ImgHeaderSize);
+            try
+            {
+                using (var stream = new MemoryStream(actual, 0, ImgConstants.ImgHeaderSize))
+                using (var writer = Factory.Create(stream, true))
+                {
+                    // Act
+                    writer.Write(imgHeader);
+                }
+
+                // Assert
+                var expected = ArrayPool<byte>.Shared.Rent(ImgConstants.ImgHeaderSize);
+                try
+                {
+                    expected[0] = 0x02;
+                    expected[1] = 0x00;
+                    expected[2] = 0x08;
+                    expected[3] = 0x00;
+                    expected[4] = 0x00;
+                    expected[5] = 0x01;
+                    expected[6] = 0x02;
+                    expected[7] = 0x00;
+                    expected[8] = 0x00;
+                    expected[9] = 0x00;
+                    expected[10] = 0x00;
+                    expected[11] = 0x05;
+                    expected[12] = 0x00;
+                    expected[13] = 0x00;
+                    expected[14] = 0x01;
+                    expected[15] = 0x00;
+                    expected[16] = 0x00;
+                    expected[17] = 0x06;
+                    expected[18] = 0x03;
+                    expected[19] = 0x00;
+
+                    Assert.Equal(new ReadOnlySpan<byte>(expected, 0, ImgConstants.ImgHeaderSize),
+                                 new ReadOnlySpan<byte>(actual, 0, ImgConstants.ImgHeaderSize));
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(expected);
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(actual);
+            }
+        }
+
+        [Fact]
+        public async Task WriteAsync_Xbox360Bc1_WritesImgHeaderToStream()
+        {
+            // Arrange
+            var imgHeader = new ImgHeader
+            {
+                Width = 512,
+                Height = 2048,
+                Depth = 1,
+                Pitch = 512,
+                TextureFormat = ImgTextureFormat.Bc1,
+                BcAlpha = 0,
+                Game = ImgGame.ConsoleGhl,
+                MipmapCount = 7,
+                Platform = ImgPlatform.Xbox360
+            };
+
+            var actual = ArrayPool<byte>.Shared.Rent(ImgConstants.ImgHeaderSize);
+            try
+            {
+                using (var stream = new MemoryStream(actual, 0, ImgConstants.ImgHeaderSize))
+                using (var writer = Factory.Create(stream, true))
+                {
+                    // Act
+                    await writer.WriteAsync(imgHeader);
+                }
+
+                // Assert
+                var expected = ArrayPool<byte>.Shared.Rent(ImgConstants.ImgHeaderSize);
+                try
+                {
+                    expected[0] = 0x02;
+                    expected[1] = 0x00;
+                    expected[2] = 0x08;
+                    expected[3] = 0x00;
+                    expected[4] = 0x00;
+                    expected[5] = 0x01;
+                    expected[6] = 0x02;
+                    expected[7] = 0x00;
+                    expected[8] = 0x00;
+                    expected[9] = 0x00;
+                    expected[10] = 0x00;
+                    expected[11] = 0x05;
+                    expected[12] = 0x00;
+                    expected[13] = 0x00;
+                    expected[14] = 0x01;
+                    expected[15] = 0x00;
+                    expected[16] = 0x00;
+                    expected[17] = 0x06;
+                    expected[18] = 0x03;
+                    expected[19] = 0x00;
+
+                    Assert.Equal(new ReadOnlySpan<byte>(expected, 0, ImgConstants.ImgHeaderSize),
+                                 new ReadOnlySpan<byte>(actual, 0, ImgConstants.ImgHeaderSize));
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(expected);
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(actual);
+            }
+        }
+    }
+}
