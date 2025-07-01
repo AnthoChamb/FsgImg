1f349583857043ddf3e131de8cf634a28ebc885a
Anthony Chamberland <anthochamberland19@outlook.com>
2025-07-01 15:04:28 -0400

    Add PowerShell module

---
+++ src/FsgImg.PowerShell/Commands/ConvertFromFsgImgDdsCommand.cs
@@ -0,0 +1,59 @@
+﻿using FsgImg.Abstractions;
+using FsgImg.Dds.Abstractions.Enums;
+using FsgImg.Dds.Abstractions.Options;
+using FsgImg.Dds.Converters;
+using FsgImg.Dds.Factories;
+using FsgImg.Factories;
+using System.IO;
+using System.Management.Automation;
+
+namespace FsgImg.PowerShell.Commands
+{
+    [Cmdlet(VerbsData.ConvertFrom, "FsgImgDds")]
+    public class ConvertFromFsgImgDdsCommand : PSCmdlet
+    {
+        [Alias("PSPath")]
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        [ValidateNotNullOrEmpty]
+        public string[] LiteralPath { get; set; }
+
+        [Parameter(Position = 1)]
+        public string Destination { get; set; }
+
+        [Parameter]
+        public DdsImgGame Game { get; set; } = DdsImgGame.ConsoleGhl;
+
+        [Parameter(Mandatory = true)]
+        public DdsImgPlatform Platform { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var options = new ConvertFromOptions
+            {
+                Game = Game,
+                Platform = Platform,
+            };
+
+            var readerFactory = new DdsStreamReaderFactory(new DdsHeaderStreamReaderFactory(new DdsHeaderBufferReaderFactory(new DdsPixelFormatBufferReaderFactory())),
+                                                           new DdsHeaderDxt10StreamReaderFactory(new DdsHeaderDxt10BufferReaderFactory()));
+            var writerFactory = new ImgHeaderStreamWriterFactory(new ImgHeaderBufferWriterFactory());
+            var converterFactory = new DdsToImgStreamConverterFactory(new DdsToImgHeaderConverter(new ImgHeaderFactory()),
+                                                                      readerFactory,
+                                                                      writerFactory,
+                                                                      new ImgStreamFactory());
+
+            foreach (var path in LiteralPath)
+            {
+                var providerPath = SessionState.Path.GetUnresolvedProviderPathFromPSPath(path);
+                var dest = string.IsNullOrEmpty(Destination) ? Path.ChangeExtension(providerPath, ImgConstants.ImgExtension) : SessionState.Path.GetUnresolvedProviderPathFromPSPath(Destination);
+
+                using (var inputStream = File.OpenRead(providerPath))
+                using (var outputStream = File.Create(dest))
+                using (var converter = converterFactory.Create(inputStream, outputStream, true))
+                {
+                    converter.ConvertFrom(options);
+                }
+            }
+        }
+    }
+}
