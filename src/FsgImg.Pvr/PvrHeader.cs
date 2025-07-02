871f86cf289f65bc5345c5eec416959aebf704f6
Anthony Chamberland <anthochamberland19@outlook.com>
2025-07-01 21:16:44 -0400

    Add PVR class library

---
+++ src/FsgImg.Pvr/PvrHeader.cs
@@ -0,0 +1,22 @@
+﻿using FsgImg.Pvr.Abstractions;
+using FsgImg.Pvr.Abstractions.Enums;
+using FsgImg.Pvr.Abstractions.Interfaces;
+
+namespace FsgImg.Pvr
+{
+    public class PvrHeader : IPvrHeader
+    {
+        public uint Version { get; set; } = PvrConstants.PvrVersion;
+        public PvrFlags Flags { get; set; }
+        public PvrPixelFormat PixelFormat { get; set; }
+        public PvrColourSpace ColourSpace { get; set; }
+        public PvrChannelType ChannelType { get; set; }
+        public uint Height { get; set; }
+        public uint Width { get; set; }
+        public uint Depth { get; set; }
+        public uint NumSurfaces { get; set; }
+        public uint NumFaces { get; set; }
+        public uint MipmapCount { get; set; }
+        public uint MetadataSize { get; set; }
+    }
+}
